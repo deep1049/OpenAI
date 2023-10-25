653c777140f9c8ec2c190e2aacf6c3d73ec76d3b
deep1049 <103635801+deep1049@users.noreply.github.com>
2023-10-26 02:16:03 +0530

    Assignment 1 done

---
+++ Backend/Routes/Basic.Routes.js
@@ -0,0 +1,75 @@
+const express = require("express");
+require("dotenv").config();
+
+const Key = process.env.Key;
+const OpenAI = require("openai");
+const openai = new OpenAI({
+  apiKey: Key,
+});
+const Router = express.Router();
+
+Router.post("/", async (req, res) => {
+  try {
+    const { topic, language, category } = req.body;
+    const messages = [
+      { role: "system", content: "You are a joker,poet and wise man" },
+      { role: "user", content: `${category} about ${topic} in ${language}` },
+    ];
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: messages,
+      temperature: 0,
+      max_tokens: 400,
+      // frequency_penalty: 0,
+      // presence_penalty: 0,
+    });
+    const result = response.choices[0];
+    res.send({ result });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+Router.post("/pincode", async (req, res) => {
+  try {
+    const userMessage = req.body.message;
+
+    const chatCompletion = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: `Pincode of ${userMessage}` }],
+      temperature: 0.6,
+      max_tokens: 30,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
+
+    // Send the response back to the client
+    res.json({ response: chatCompletion.choices[0].message.content });
+  } catch (error) {
+    // Handle any errors that occur during the request
+    console.error("Error:", error);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+Router.post("/state", async (req, res) => {
+  try {
+    const userMessage = req.body.message;
+
+    const chatCompletion = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: `Area of ${userMessage} in Sqkm` }],
+      temperature: 1,
+      max_tokens: 30,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
+
+    res.json({ response: chatCompletion.choices[0].message.content });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+module.exports = { Router };
---
+++ Backend/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { Router } = require("./Routes/Basic.Routes");
+
+const app = express();
+
+require("dotenv").config();
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use("/basic", Router);
+
+const port = process.env.port;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
